fce49750a24ec25132ea9815c7e1ea21b7c5fde5
shubhamdogra418 <shubhamdogra418@gmail.com>
2024-01-03 23:35:20 +0530

    button counter

---
+++ counter.js
@@ -0,0 +1,20 @@
+let count=0;
+// document.getElementById('decrease').onclick= function () {
+//     count-=1;
+//     document.getElementById('label').innerHTML=count;
+// }
+document.getElementById('increase').onclick= function () {
+    document.getElementById('label').innerHTML=count;
+
+}
+document.getElementById('reset').onclick= function () {
+    count=0;
+    document.getElementById('label').innerHTML=count;
+
+}
+
+document.getElementById('increase').addEventListener('click', function () {
+    count+=1;
+    document.getElementById('label').innerHTML=count;
+})
+
