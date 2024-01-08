206214da5913d272618dce786ab60e777867e426
shubhamdogra418 <shubhamdogra418@gmail.com>
2024-01-08 17:06:07 +0530

    map, sets, arrays, objects

--- mapset.js
+++ mapset.js
@@ -6,99 +6,62 @@
 
 // console.log(map.get('love birds'));
 
-// const promise = new Promise((resolve, reject) => {
-//     let speed= 2;
-//     console.log(speed);
-//     if(speed > 5) {
-//         resolve("downloaded successfully");
-//     } else {
-//         reject("low speed");
-//     }
-// })
-//  .then((result)=> {
-//     console.log("success")
-//  })
-//  .catch((result) => {
-//     console.error(new Error("failed"))
-// })
+// let arr=[1,2,3,3,5,2,1];
+// let mySet= new Set(arr); //takes only unique elements
+
+// // Q. delete all dupliacte valuse from an array
+// const uniqueEls= [...mySet]; //spread operator (expands, rest- shrinks, condense)
+// console.log(uniqueEls);
+// // console.log(arr);
+// // console.log(set);
+
+// //ADD
+// console.log(mySet.add(21));
+// console.log(mySet.add("shubham")); //add string
+// console.log(mySet.add('21')); //add chaarcater
+// let newArr= [98,89,87,78];
+// console.log(mySet.add(newArr)); //adds array
+// console.log(mySet.add({ name: "shubham"})); // add object
+// console.log(mySet.add(false));
+
+// //DELETE
+// console.log(mySet.delete(2)); //returns boolean
+// console.log(mySet.delete({name}));
+// console.log(mySet.delete(false));
+// console.log(mySet.delete(newArr)); //n JS object types are always referenced while primitive types are not.
+
+// // mySet.clear(); //delete all the elements
+
+// console.log("has", mySet.has("shubham"));
+// console.log(mySet.has("neh"));
+
+// //SIZE of set
+// console.log(mySet.size);
+
+// console.log(mySet.keys());
+// console.log(mySet.values());
+// console.log(mySet.entries());
 
 
+//MAP
+const myMap= new Map([ ['name', 'shubham'], ['place', 'himachalpradesh']]);
+myMap.set('hobby', 'travel');
+myMap.set({name: 'shubham'}, "apple"); //add-set
+console.log(myMap.delete('hobby')); //delete
+// myMap.clear(); //clear all
+console.log(myMap.has('name')); //return boolean only for key and not values
+console.log(myMap.has('shubham')); // has method not for values
+console.log(myMap.size); //return size
+console.log(myMap.get('name'));
+myMap.forEach ((value, key) => {
+    console.log(key,value);
+});
 
-// const div = document.getElementById('parent');
-// function changeColor (color, delay) {
-//     return new Promise((resolve, reject) => {
-//         div.style.backgroundColor= color;
-//         setTimeout(() => {
-//             resolve("color changed")
-//         }, delay);
-//     })
-// }
-
-// changeColor("green",2000);
-// //ASYNC AWAIT
-//     async function demo() {
-//         await changeColor ("red",3000);
-//         await changeColor ("blue",1000);
-//         await changeColor ("green",1000);
-//         await changeColor ("yellow",1000);
-//         await changeColor ("black",1000);
-//         await changeColor ("skyblue",1000);
-//         await changeColor ("brown",1000);
-//         await changeColor ("tomato",1000);
-//         changeColor("gray",1000);
-//     }
-//     demo();
+console.log(Array.from(myMap.keys()));
+console.log(Array.from(myMap.values()));
 
 
-//PROMISE CHAINING
-
-// changeColor("red",1000)
-// .then((result) => {
-//     console.log("1st done", result);
-//     return changeColor("blue",3000);
-// })
-// .then((result) => {
-//     console.log("2nd done", result);
-//     return changeColor("yellow", 1000);
-// })
-// .then((result)=> {
-//     console.log("3rd done", result);
-//     return changeColor("green",1000);
-// })
-// .catch((result) => {
-//     console.error(new Error(result));
-// })
-// .finally("task has been done, final cleanup");
-
-
-
-//getting the API data
-const h2= document.getElementById("h2");
-const head2= document.getElementById("head2");
-
-// async function getCatFact () {
-//     const data= await fetch("https://catfact.ninja/fact");
-//     const json= await data.json();
-//     console.log(json);
-//     h2.innerText= json.fact;
-    
-//     try {
-//         const data2= await fetch("https://catfact.ninja/fact");
-//         const json2= await data2.json();
-//         console.log(json2);
-//         head2.innerText= json2.fact;
-
-//     }
-//     catch(err) {
-//         console.log(err);
-//     }
-//     finally {
-//         console.log('all data fetched');
-//     }
-// }
-
-// getCatFact();
-
+// console.log(myMap);
 
 
 
